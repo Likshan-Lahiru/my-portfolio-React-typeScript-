da83e9c975bfe0a95b7de5ffeb284de905384b5b
Lahiru_Lolitha <lahiru212001@gmail.com>
2025-03-05 22:53:47 +0530

    update

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -7,21 +7,21 @@ export const TechStack: React.FC = () => {
     isInView
   } = useScrollAnimation();
   const technologies =
-      [  { name: "HTML5", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
+      [   { name: "Java", image: "https://i.ibb.co/gbb2rb1H/pngwing-com-3.png" },
+          { name: "Python", image: "https://i.ibb.co/KzwTtRJ2/pngwing-com-4.png" },
+          { name: "AWS", image: "https://i.ibb.co/4ZrPLyXX/abf067f3bef03527c829cc1ee52768d5.png" },
+          { name: "Azure", image: "https://i.ibb.co/21kBsnRH/756775c0e4483c74bc9ffbb730e3fab2.png" },
+          { name: "React", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
+          { name: "HTML5", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
           { name: "CSS3", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" },
           { name: "JavaScript", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
-          { name: "React", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
           { name: "Redux", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" },
           { name: "Bootstrap", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" },
           { name: "Tailwind CSS", image: "https://i.ibb.co/4wqBBrTn/pngwing-com-1.png" },
           { name: "SASS", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" },
           { name: "Spring Boot", image: "https://i.ibb.co/999L678h/pngaaa-com-2459546.png" },
           { name: "Node.js", image: "https://i.ibb.co/rGGVFXd9/pngwing-com-2.png" },
-          { name: "Java", image: "https://i.ibb.co/gbb2rb1H/pngwing-com-3.png" },
-          { name: "Python", image: "https://i.ibb.co/KzwTtRJ2/pngwing-com-4.png" },
           { name: "TypeScript", image: "https://i.ibb.co/FrB06gr/pngwing-com-5.png" },
-          { name: "AWS", image: "https://i.ibb.co/4ZrPLyXX/abf067f3bef03527c829cc1ee52768d5.png" },
-          { name: "Azure", image: "https://i.ibb.co/21kBsnRH/756775c0e4483c74bc9ffbb730e3fab2.png" },
           { name: "Git", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" },
           { name: "GitHub", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" },
           { name: "Figma", image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" }];
